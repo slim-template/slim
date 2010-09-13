03fcbcc33eb9276e5dd127cc1d72f7976129d309
stonean <andy@stonean.com>
2010-09-13 00:05:21 -0400

    use heredoc without dash to simulate file indentation more precisely

--- test/slim/test_precompiler.rb
+++ test/slim/test_precompiler.rb
@@ -32,14 +32,14 @@ class TestSlimEngine < MiniTest::Unit::TestCase
   end
 
   def test_simple_html_with_params
-    string = <<-HTML
-    html 
-      head
-        title Simple Test Title
-        meta name="description" content="This is a Slim Test, that's all"
-      body
-        p Hello World, meet Slim.
-    HTML
+    string = <<HTML
+html 
+  head
+    title Simple Test Title
+    meta name="description" content="This is a Slim Test, that's all"
+  body
+    p Hello World, meet Slim.
+HTML
 
     expected = %q|_buf = [];_buf << "<html>";_buf << "<head>";_buf << "<title>";_buf << "Simple Test Title";_buf << "</title>";_buf << "<meta name=\"description\" content=\"This is a Slim Test, that's all\"/>";_buf << "</head>";_buf << "<body>";_buf << "<p>";_buf << "Hello World, meet Slim.";_buf << "</p>";_buf << "</body>";_buf << "</html>";_buf.join;|
 
@@ -49,14 +49,14 @@ class TestSlimEngine < MiniTest::Unit::TestCase
   end
 
   def test_simple_html_with_params_meta_first
-    string = <<-HTML
-    html 
-      head
-        meta name="description" content="This is a Slim Test, that's all"
-        title Simple Test Title
-      body
-        p Hello World, meet Slim.
-    HTML
+    string = <<HTML
+html 
+  head
+    meta name="description" content="This is a Slim Test, that's all"
+    title Simple Test Title
+  body
+    p Hello World, meet Slim.
+HTML
 
     expected = %q|_buf = [];_buf << "<html>";_buf << "<head>";_buf << "<meta name=\"description\" content=\"This is a Slim Test, that's all\"/>";_buf << "<title>";_buf << "Simple Test Title";_buf << "</title>";_buf << "</head>";_buf << "<body>";_buf << "<p>";_buf << "Hello World, meet Slim.";_buf << "</p>";_buf << "</body>";_buf << "</html>";_buf.join;|
 
@@ -64,4 +64,23 @@ class TestSlimEngine < MiniTest::Unit::TestCase
 
     assert_equal expected, output
   end
+
+  def test_nested_content
+    string = <<HTML
+html 
+  head
+    meta name="description" content="This is a Slim Test, that's all"
+    title Simple Test Title
+  body
+    p 
+      Hello World, meet Slim.
+HTML
+
+    expected = %q|_buf = [];_buf << "<html>";_buf << "<head>";_buf << "<meta name=\"description\" content=\"This is a Slim Test, that's all\"/>";_buf << "<title>";_buf << "Simple Test Title";_buf << "</title>";_buf << "</head>";_buf << "<body>";_buf << "<p>";_buf << "      Hello World, meet Slim.";_buf << "</p>";_buf << "</body>";_buf << "</html>";_buf.join;|
+
+    output = TestEngine.new(string).precompiled
+
+    assert_equal expected, output
+  end
+
 end
