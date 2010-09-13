7ebe314b289244daee74c0cea5d67ab54a3ccb95
stonean <andy@stonean.com>
2010-09-13 00:17:51 -0400

    tests for self closing tags

--- test/slim/test_precompiler.rb
+++ test/slim/test_precompiler.rb
@@ -83,4 +83,44 @@ HTML
     assert_equal expected, output
   end
 
+  def test_closing_tag_without_content_or_attributes
+    string = <<HTML
+html 
+  head
+    meta name="description" content="This is a Slim Test, that's all"
+    title Simple Test Title
+  body
+    hr
+    p 
+      Hello World, meet Slim.
+HTML
+
+    expected = %q|_buf = [];_buf << "<html>";_buf << "<head>";_buf << "<meta name=\"description\" content=\"This is a Slim Test, that's all\"/>";_buf << "<title>";_buf << "Simple Test Title";_buf << "</title>";_buf << "</head>";_buf << "<body>";_buf << "<hr/>";_buf << "<p>";_buf << "      Hello World, meet Slim.";_buf << "</p>";_buf << "</body>";_buf << "</html>";_buf.join;|
+
+    output = TestEngine.new(string).precompiled
+
+    assert_equal expected, output
+  end
+
+  def test_closing_tag_without_content
+    string = <<HTML
+html 
+  head
+    meta name="description" content="This is a Slim Test, that's all"
+    title Simple Test Title
+  body
+    img width="100" height="50" src="/images/test.jpg"
+    p 
+      Hello World, meet Slim.
+HTML
+
+    expected = %q|_buf = [];_buf << "<html>";_buf << "<head>";_buf << "<meta name=\"description\" content=\"This is a Slim Test, that's all\"/>";_buf << "<title>";_buf << "Simple Test Title";_buf << "</title>";_buf << "</head>";_buf << "<body>";_buf << "<img width=\"100\" height=\"50\" src=\"/images/test.jpg\"/>";_buf << "<p>";_buf << "      Hello World, meet Slim.";_buf << "</p>";_buf << "</body>";_buf << "</html>";_buf.join;|
+
+    output = TestEngine.new(string).precompiled
+
+    assert_equal expected, output
+  end
+
+
+
 end
