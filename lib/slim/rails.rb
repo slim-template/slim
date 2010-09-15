a94f64e3a8e6291fea78728d671c11ad87af75ea
stonean <andy@stonean.com>
2010-09-15 18:08:53 -0400

    rails hook

---
+++ lib/slim/rails.rb
@@ -0,0 +1,15 @@
+require 'slim'
+
+module ActionView
+  module TemplateHandlers
+    class SlimHandler < TemplateHandler
+      include Compilable
+
+      def compile(template)
+        return Slim::Engine.new(template.source).compiled
+      end
+    end
+  end
+
+  Template.register_default_template_handler :slim, TemplateHandlers::SlimHandler
+end
