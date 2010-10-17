c12b8a2149bda18b379baf9c9d558963d15dfbe6
stonean <andy@stonean.com>
2010-10-17 12:32:49 -0400

    doh. slim vs. erb was wrong since slim escapes html by default. modified the tests so they function the same.

--- benchmarks/run.rb
+++ benchmarks/run.rb
@@ -2,11 +2,11 @@
 
 require File.dirname(__FILE__) + '/src/helper'
 require File.dirname(__FILE__) + '/src/complex_view'
+require File.dirname(__FILE__) + '/../lib/slim'
 
 require 'ostruct'
 require 'erb'
 require 'haml'
-require 'slim'
 require 'mustache'
 
 tpl_erb      = File.read(File.dirname(__FILE__) + '/src/complex.erb')
@@ -23,6 +23,14 @@ slim              = Slim::Engine.new(tpl_slim)
 mustache          = Mustache.new
 mustache.template = tpl_mustache
 
+mustache[:header] = 'Colors'
+items = []
+items << { :name => 'red', :current => true, :url => '#Red' }
+items << { :name => 'green', :current => false, :url => '#Green' }
+items << { :name => 'blue', :current => false, :url => '#Blue' }
+
+mustache[:item] = items
+
 bench('erb')               { ERB.new(tpl_erb).result(eview) }
 bench('slim')              { Slim::Engine.new(tpl_slim).render(view) }
 bench('haml')              { Haml::Engine.new(tpl_haml).render(view) }
@@ -30,4 +38,4 @@ bench('mustache')          { Mustache.render(tpl_mustache, view) }
 bench('erb (cached)')      { erb.result(eview) }
 bench('slim (cached)')     { slim.render(view) }
 bench('haml (cached)')     { haml.render(view) }
-bench('mustache (cached)') { mustache.render(mustache.template, view) }
+bench('mustache (cached)') { mustache.render }
