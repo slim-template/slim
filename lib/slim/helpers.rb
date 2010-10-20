d2345ffd787217969b27c16b0136c5d13f3d71ed
Fred Wu <fred@envato.com>
2010-10-20 14:08:14 +1100

    Cleaned up escape_html.

--- lib/slim/helpers.rb
+++ lib/slim/helpers.rb
@@ -12,24 +12,18 @@ module Slim
       escape_html(html)
     end
 
-    if defined?(EscapeUtils)
-      def escape_html(html)
+    def escape_html(html)
+      if defined?(EscapeUtils)
         EscapeUtils.escape_html(html.to_s)
-      end
-    elsif RUBY_VERSION > '1.9'
-      ESCAPE_HTML = {
-        '&' => '&amp;',
-        '"' => '&quot;',
-        '<' => '&lt;',
-        '>' => '&gt;',
-        '/' => '&#47;',
-      }
-
-      def escape_html(html)
-        html.to_s.gsub(/[&\"<>\/]/, ESCAPE_HTML)
-      end
-    else
-      def escape_html(html)
+      elsif RUBY_VERSION > '1.9'
+        html.to_s.gsub(/[&\"<>\/]/, {
+          '&' => '&amp;',
+          '"' => '&quot;',
+          '<' => '&lt;',
+          '>' => '&gt;',
+          '/' => '&#47;',
+        })
+      else
         html.to_s.gsub(/&/n, '&amp;').gsub(/\"/n, '&quot;').gsub(/>/n, '&gt;').gsub(/</n, '&lt;').gsub(/\//, '&#47;')
       end
     end
