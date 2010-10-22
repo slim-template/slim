af85555fa9e96b63d4978cc52389bf0c30038fe5
minad <mail@daniel-mendler.de>
2010-10-22 23:02:46 +0800

    precompile embedded engine if engine supports it

--- test/slim/test_embedded_engines.rb
+++ test/slim/test_embedded_engines.rb
@@ -5,12 +5,14 @@ class TestSlimEmbeddedEngines < TestSlim
   def test_render_with_embedded_template
     source = %q{
 p
+  - text = 'before erb block'
   erb:
-    <b>Hello from <%= 'erb'.upcase %>!</b>
+    <b>Hello from <%= text.upcase %>!</b>
     Second Line!
+    <% if true %><%= true %><% end %>
 }
 
-    assert_html "<p><b>Hello from ERB!</b>\nSecond Line!\n</p>", source
+    assert_html "<p><b>Hello from BEFORE ERB BLOCK!</b>\nSecond Line!\ntrue\n</p>", source
   end
 
   def test_render_with_interpolated_embedded_template
@@ -39,5 +41,15 @@ ruby:
 = variable
 }
     assert_html '3', source
+  end
+
+  def test_render_with_liquid
+    source = %q{
+p
+  - text = 'before liquid block'
+  liquid:
+    <span>{{text}}</span>
+}
+    assert_html "<p><span>before liquid block</span>\n</p>", source
   end
 end
