eaef5d9516e80f762efef121391d3c80ccb56522
minad <mail@daniel-mendler.de>
2010-11-01 03:00:38 +0100

    use pretty html filter

--- lib/slim/engine.rb
+++ lib/slim/engine.rb
@@ -9,10 +9,10 @@ module Slim
     use Slim::EmbeddedEngine
     use Slim::Compiler
     filter :Debugger, :debug, :prefix => 'after compilation'
-    use Temple::HTML::Fast, :format, :attr_wrapper, :id_delimiter, :id_concat,
-                            :attr_wrapper => '"', :format => :html5, :id_delimiter => nil
-    filter :Debugger, :debug, :prefix => 'after html'
     filter :EscapeHTML, :use_html_safe
+    use Temple::HTML::Pretty, :format, :attr_wrapper, :id_delimiter, :id_concat, :pretty,
+                              :pretty => false, :attr_wrapper => '"', :format => :html5, :id_delimiter => nil
+    filter :Debugger, :debug, :prefix => 'after html'
     filter :MultiFlattener
     filter :StaticMerger
     filter :DynamicInliner
