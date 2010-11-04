51aeb4cf25e2f0bcb30e7b922fea5c08fe724e96
stonean <andy@stonean.com>
2010-11-03 21:06:26 -0400

    expose options for ease of use.  thinking rails environment files here...

--- lib/slim/engine.rb
+++ lib/slim/engine.rb
@@ -2,6 +2,21 @@ module Slim
   # Slim engine which transforms slim code to executable ruby code
   # @api public
   class Engine < Temple::Engine
+    # Allow users to set options, particularly useful in Rails' environment files.
+    # For instance, in config/environments/development.rb you probably want:
+    #     # Indent html for pretty debugging
+    #     Slim::Engine.options[:pretty] = true
+    #
+    # @return [Hash] options
+    def self.options
+      @options ||= {}
+    end
+
+    options[:pretty]        = false 
+    options[:attr_wrapper]  = '"' 
+    options[:format]        = :html5 
+    options[:id_delimiter]  = nil
+
     use Slim::Parser, :file
     use Slim::EmbeddedEngine
     use Slim::Interpolation
@@ -9,11 +24,14 @@ module Slim
     use Slim::EndInserter
     use Slim::Compiler
     filter :EscapeHTML, :use_html_safe
-    use Temple::HTML::Pretty, :format, :attr_wrapper, :id_delimiter, :id_concat, :pretty,
-                              :pretty => false, :attr_wrapper => '"', :format => :html5, :id_delimiter => nil
+    use Temple::HTML::Pretty, :format, :attr_wrapper, :id_delimiter, :id_concat, :pretty
     filter :MultiFlattener
     filter :StaticMerger
     filter :DynamicInliner
     generator :ArrayBuffer
+
+    def initialize(options = {})
+      super(self.class.options.merge(options))
+    end
   end
 end
