ea7c63c241feb9a5d2b392171bda373f520dea75
minad <mail@daniel-mendler.de>
2010-11-08 14:47:43 +0100

    get rid of rails deprecation warning

--- lib/slim/rails.rb
+++ lib/slim/rails.rb
@@ -2,12 +2,23 @@ require 'slim'
 
 module ActionView
   module TemplateHandlers
-    # Slim handler for Rails 3
-    class SlimHandler < TemplateHandler
-      include Compilable
+    raise "Slim supports only Rails 3.x and greater, your Rails version is #{Rails::VERSION::STRING}" if Rails::VERSION::MAJOR < 3
 
-      def compile(template)
-        Slim::Engine.new.compile(template.source)
+    if Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR < 1
+      # Slim handler for Rails 3.0
+      class SlimHandler < TemplateHandler
+        include Compilable
+
+        def compile(template)
+          Slim::Engine.new.compile(template.source)
+        end
+      end
+    else
+      # Slim handler for Rails 3.1 and greater
+      class SlimHandler
+        def call(template)
+          Slim::Engine.new.compile(template.source)
+        end
       end
     end
   end
