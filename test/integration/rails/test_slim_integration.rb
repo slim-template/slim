897b9d400b78bb6af0d2f7cc36b905ffa34193ac
Fred Wu <ifredwu@gmail.com>
2010-12-21 11:32:38 +1100

    Corrected the failing test.

--- test/integration/rails/test_slim_integration.rb
+++ test/integration/rails/test_slim_integration.rb
@@ -9,7 +9,8 @@ class TestSlimIntegration < TestSlimRails
     fill_in "parent_children_attributes_0_name", :with => "c1"
     click_button "Create Parent"
     click_link "Edit"
-    assert_equal true, page.has_content?("input#parent_children_attributes_0_id")
+    assert_equal true, page.has_css?("input#parent_children_attributes_0_name")
+    assert_equal true, page.has_css?("input#parent_children_attributes_0_id")
     click_button "Update Parent"
     assert_equal 1, Child.count
   end
