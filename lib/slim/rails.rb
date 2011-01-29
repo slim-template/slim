9782dc772c4a53c5c28c2fc7f60b92d6d4a216ea
stonean <andy@stonean.com>
2011-01-29 15:43:31 -0500

    slim should not be registered as the default template handler.

--- lib/slim/rails.rb
+++ lib/slim/rails.rb
@@ -5,7 +5,9 @@ Slim::Engine.default_options[:disable_capture] = true
 
 module ActionView
   module TemplateHandlers
-    raise "Slim supports only Rails 3.x and greater, your Rails version is #{Rails::VERSION::STRING}" if Rails::VERSION::MAJOR < 3
+    if Rails::VERSION::MAJOR < 3
+      raise "Slim supports only Rails 3.x and greater, your Rails version is #{Rails::VERSION::STRING}"
+    end
 
     if Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR < 1
       # Slim handler for Rails 3.0
@@ -26,5 +28,5 @@ module ActionView
     end
   end
 
-  Template.register_default_template_handler :slim, TemplateHandlers::SlimHandler
+  Template.register_template_handler :slim, TemplateHandlers::SlimHandler
 end
