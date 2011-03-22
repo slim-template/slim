49b3c46b5bcf89c7552af85c733526fc1ca60879
minad <mail@daniel-mendler.de>
2011-03-22 16:43:36 +0100

    use slim instead of slim/rails

--- test/integration/rails/dummy/config/application.rb
+++ test/integration/rails/dummy/config/application.rb
@@ -1,12 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
 
-require "slim/rails"
+require 'slim'
 
 module Dummy
   class Application < Rails::Application
