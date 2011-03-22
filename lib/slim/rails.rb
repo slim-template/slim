f828cb6c9e56ffa3183731ef3e4c2f7635e21521
minad <mail@daniel-mendler.de>
2011-03-22 15:54:18 +0100

    rails autodetect
    `require 'slim'` suffices now

--- lib/slim/rails.rb
+++ lib/slim/rails.rb
@@ -1,11 +1,2 @@
 require 'slim'
-
-module Slim
-  class RailsTemplate < Temple::Templates::Rails
-    engine Slim::Engine
-    register :slim
-
-    set_default_options :generator => Temple::Generators::RailsOutputBuffer,
-                        :disable_capture => true
-  end
-end
+puts 'Deprecated: Use `require \'slim\'` instead of `require \'slim/rails\'`'
--- lib/slim/template.rb
+++ lib/slim/template.rb
@@ -5,4 +5,16 @@ module Slim
     engine Slim::Engine
     register :slim
   end
+
+  if Kernel.const_defined?(:Rails)
+    # Rails template implementation for Slim
+    # @api public
+    class RailsTemplate < Temple::Templates::Rails
+      engine Slim::Engine
+      register :slim
+
+      set_default_options :generator => Temple::Generators::RailsOutputBuffer,
+                          :disable_capture => true
+    end
+  end
 end
