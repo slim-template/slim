390790a23a39fd173b2df50158b56443a697e227
minad <mail@daniel-mendler.de>
2011-03-24 17:55:24 +0100

    temple supports denser template registration

--- lib/slim/template.rb
+++ lib/slim/template.rb
@@ -1,29 +1,17 @@
 module Slim
   # Tilt template implementation for Slim
   # @api public
-  class Template < Temple::Templates::Tilt
-    # Use the Slim::Engine for this template
-    engine Slim::Engine
-
-    # Register this template for *.slim files in Tilt
-    register :slim
-  end
+  Template = Temple::Templates::Tilt(Slim::Engine, :register_as => :slim)
 
   if Kernel.const_defined?(:Rails)
     # Rails template implementation for Slim
     # @api public
-    class RailsTemplate < Temple::Templates::Rails
-      # Use the Slim::Engine for this template
-      engine Slim::Engine
+    RailsTemplate = Temple::Templates::Rails(Slim::Engine, :register_as => :slim)
 
-      # Register this template for *.slim files in Rails
-      register :slim
-
-      # Use rails-specific generator. This is necessary
-      # to support block capturing. Disable the internal slim capturing.
-      # Rails takes care of the capturing by itself.
-      set_default_options :generator => Temple::Generators::RailsOutputBuffer,
-                          :disable_capture => true
-    end
+    # Use rails-specific generator. This is necessary
+    # to support block capturing. Disable the internal slim capturing.
+    # Rails takes care of the capturing by itself.
+    RailsTemplate.set_default_options :generator => Temple::Generators::RailsOutputBuffer,
+                                      :disable_capture => true
   end
 end
