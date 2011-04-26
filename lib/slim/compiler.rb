79776c2be29f8c16062b47a6cf88e1a0e06b23a5
minad <mail@daniel-mendler.de>
2011-04-26 06:28:06 +0200

    remove on_slim_output_block

--- lib/slim/compiler.rb
+++ lib/slim/compiler.rb
@@ -42,40 +42,29 @@ module Slim
       if empty_exp?(content)
         [:multi, [:escape, escape && options[:auto_escape], [:dynamic, code]], content]
       else
-        on_slim_output_block(escape, code, content)
+        tmp = tmp_var(:output)
+
+        [:multi,
+         # Capture the result of the code in a variable. We can't do
+         # `[:dynamic, code]` because it's probably not a complete
+         # expression (which is a requirement for Temple).
+         [:block, "#{tmp} = #{code}"],
+
+         # Capture the content of a block in a separate buffer. This means
+         # that `yield` will not output the content to the current buffer,
+         # but rather return the output.
+         #
+         # The capturing can be disabled with the option :disable_capture.
+         # Output code in the block writes directly to the output buffer then.
+         # Rails handles this by replacing the output buffer for helpers (with_output_buffer - braindead!).
+         options[:disable_capture] ? compile(content) : [:capture, tmp_var(:output), compile(content)],
+
+         # Close the block.
+         [:block, 'end'],
+
+         # Output the content.
+         on_slim_output(escape, tmp, [:multi])]
       end
-    end
-
-    # Handle output expression `[:slim, :output, escape, code, content]`
-    # if content is not empty.
-    #
-    # @param [Boolean] escape Escape html
-    # @param [String] code Ruby code
-    # @param [Array] content Temple expression
-    # @return [Array] Compiled temple expression
-    def on_slim_output_block(escape, code, content)
-      tmp = tmp_var(:output)
-
-      [:multi,
-        # Capture the result of the code in a variable. We can't do
-        # `[:dynamic, code]` because it's probably not a complete
-        # expression (which is a requirement for Temple).
-        [:block, "#{tmp} = #{code}"],
-
-        # Capture the content of a block in a separate buffer. This means
-        # that `yield` will not output the content to the current buffer,
-        # but rather return the output.
-        #
-        # The capturing can be disabled with the option :disable_capture.
-        # Output code in the block writes directly to the output buffer then.
-        # Rails handles this by replacing the output buffer for helpers (with_output_buffer - braindead!).
-        options[:disable_capture] ? compile(content) : [:capture, tmp_var(:output), compile(content)],
-
-        # Close the block.
-        [:block, 'end'],
-
-        # Output the content.
-        on_slim_output(escape, tmp, [:multi])]
     end
 
     # Handle directive expression `[:slim, :directive, type, args]`
