35e5658f0fd1026e7d1d1bca19c0fcc161d530de
minad <mail@daniel-mendler.de>
2011-04-30 07:29:54 +0200

    slim grammar improved

--- lib/slim/grammar.rb
+++ lib/slim/grammar.rb
@@ -3,15 +3,14 @@ module Slim
     extend Temple::Grammar
 
     Expression <<
-      [:slim, :control, String, 'Expression']        |
-      [:slim, :condcomment, String, 'Expression']    |
-      [:slim, :output, Bool, String, 'Expression'] |
-      [:slim, :text, String]                         |
-      [:slim, :embedded, String, 'Expression']       |
-      [:slim, :directive, Value('doctype'), String]  |
-      [:html, :attrs, 'SlimAttr*']
-    SlimAttr <<
-      [:slim, :attr, String, 'Bool', String] |
-      'HTMLAttr'
+      [:slim, :control, String, 'Expression']       |
+      [:slim, :condcomment, String, 'Expression']   |
+      [:slim, :output, Bool, String, 'Expression']  |
+      [:slim, :text, String]                        |
+      [:slim, :embedded, String, 'Expression']      |
+      [:slim, :directive, Value('doctype'), String]
+
+    HTMLAttr <<
+      [:slim, :attr, String, 'Bool', String]
   end
 end
