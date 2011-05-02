91560c771a628fdf5b1b5d4a649329a4db8029a0
minad <mail@daniel-mendler.de>
2011-05-02 15:54:52 +0200

    pass options to class generator

--- lib/slim/template.rb
+++ lib/slim/template.rb
@@ -3,15 +3,13 @@ module Slim
   # @api public
   Template = Temple::Templates::Tilt(Slim::Engine, :register_as => :slim)
 
-  if Object.const_defined?(:Rails)
-    # Rails template implementation for Slim
-    # @api public
-    RailsTemplate = Temple::Templates::Rails(Slim::Engine, :register_as => :slim)
-
-    # Use rails-specific generator. This is necessary
-    # to support block capturing. Disable the internal slim capturing.
-    # Rails takes care of the capturing by itself.
-    RailsTemplate.set_default_options :generator => Temple::Generators::RailsOutputBuffer,
-                                      :disable_capture => true
-  end
+  # Rails template implementation for Slim
+  # @api public
+  RailsTemplate = Temple::Templates::Rails(Slim::Engine,
+                                           :register_as => :slim,
+                                           # Use rails-specific generator. This is necessary
+                                           # to support block capturing. Disable the internal slim capturing.
+                                           # Rails takes care of the capturing by itself.
+                                           :generator => Temple::Generators::RailsOutputBuffer,
+                                           :disable_capture => true) if Object.const_defined?(:Rails)
 end
