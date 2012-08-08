4aa41cfb9447f6191526ec24a466abf0956cab53
Daniel Mendler <mail@daniel-mendler.de>
2012-08-08 20:46:06 +0200

    add test case for #281

--- test/slim/test_encoding.rb
+++ test/slim/test_encoding.rb
@@ -1,6 +1,12 @@
 require 'helper'
 
 class TestSlimEncoding < TestSlim
+  def test_windows_crlf
+    source = "a href='#' something\r\nbr\r\na href='#' others\r\n"
+    result = "<a href=\"#\">something</a><br /><a href=\"#\">others</a>"
+    assert_html result, source
+  end
+
   def test_binary
     source = "| \xFF\xFF"
     result = "\xFF\xFF"
