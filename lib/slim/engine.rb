526eb32d775ca9bb88279394fa0d72ece851a1a0
Daniel Mendler <mail@daniel-mendler.de>
2012-09-03 21:52:26 +0200

    remove documentation from engine
    avoid duplication with README.md and avoid synchronization issues

--- lib/slim/engine.rb
+++ lib/slim/engine.rb
@@ -2,12 +2,9 @@ module Slim
   # Slim engine which transforms slim code to executable ruby code
   # @api public
   class Engine < Temple::Engine
-    # Allow users to set default options, particularly useful in Rails' environment files.
-    # For instance, in config/environments/development.rb you probably want:
-    #     # Indent html for pretty debugging
-    #     Slim::Engine.set_default_options :pretty => true
-    #
-    # This overwrites some temple default options.
+    # This overwrites some Temple default options or sets default options for Slim specific filters.
+    # It is recommended to set the default settings only once in the code and avoid duplication. Only use
+    # `set_default_options` when you have to override some default settings.
     set_default_options :pretty => false,
                         :sort_attrs => true,
                         :attr_wrapper => '"',
@@ -15,47 +12,7 @@ module Slim
                         :remove_empty_attrs => true,
                         :generator => Temple::Generators::ArrayBuffer,
                         :default_tag => 'div'
-    #
-    # Document all supported options with purpose, type etc.
-    #
-    # Type        | Name               | Default value                 | Purpose
-    # --------------------------------------------------------------------------------------------------------------------------------------------
-    # String      | :file              | nil                           | Name of parsed file, set automatically by Slim::Template
-    # Integer     | :tabsize           | 4                             | Number of whitespaces per tab (used by the parser)
-    # String      | :encoding          | "utf-8"                       | Set encoding of template
-    # String      | :default_tag       | "div"                         | Default tag to be used if tag name is omitted
-    # Hash        | :shortcut          | {'.' => 'class', ...}         | Attribute shortcuts
-    # String list | :enable_engines    | All enabled                   | List of enabled embedded engines (whitelist)
-    # String list | :disable_engines   | None disabled                 | List of disabled embedded engines (blacklist)
-    # Boolean     | :disable_capture   | false (true in Rails)         | Disable capturing in blocks (blocks write to the default buffer
-    # Boolean     | :disable_escape    | false                         | Disable automatic escaping of strings
-    # Boolean     | :use_html_safe     | false (true in Rails)         | Use String#html_safe? from ActiveSupport (Works together with :disable_escape)
-    # Symbol      | :format            | :xhtml                        | HTML output format
-    # String      | :attr_wrapper      | '"'                           | Character to wrap attributes in html (can be ' or ")
-    # Hash        | :attr_delimiter    | {'class' => ' '}              | Joining character used if multiple html attributes are supplied (e.g. id1_id2)
-    # Boolean     | :sort_attrs        | true                          | Sort attributes by name
-    # Boolean     | :remove_empty_attrs| true                          | Remove attributes with empty value
-    # Boolean     | :pretty            | false                         | Pretty html indenting (This is slower!)
-    # String      | :indent            | '  '                          | Indentation string
-    # Boolean     | :streaming         | false (true in Rails > 3.1)   | Enable output streaming
-    # Class       | :generator         | ArrayBuffer/RailsOutputBuffer | Temple code generator (default generator generates array buffer)
-    #
-    # It is also possible to set all options supported by the generator (option :generator). The standard generators
-    # support the options :buffer and :capture_generator.
-    #
-    # Options can be set at multiple positions. Slim/Temple uses a inheritance mechanism to allow
-    # subclasses to overwrite options of the superclass. The option priorities are as follows:
-    #
-    # Custom (Options passed by the user) > Slim::Template > Slim::Engine > Parser/Filter/Generator (e.g Slim::Parser, Slim::Compiler)
-    #
-    # It is also possible to set options for superclasses like Temple::Engine. But this will affect all temple template engines then.
-    #
-    # Slim::Engine > Temple::Engine
-    # Slim::Compiler > Temple::Filter
-    #
-    # It is recommended to set the default settings only once in the code and avoid duplication. Only use
-    # `set_default_options` when you have to override some default settings.
-    #
+
     use Slim::Parser, :file, :tabsize, :encoding, :shortcut, :default_tag
     use Slim::EmbeddedEngine, :enable_engines, :disable_engines, :pretty
     use Slim::Interpolation
