1c9b7d35d99f89aaac51c5ee636ab1a76e6746f9
Daniel Mendler <mail@daniel-mendler.de>
2012-09-29 13:43:12 +0200

    use temple for the literatetest compiler :-)

--- test/literate/run.rb
+++ test/literate/run.rb
@@ -1,81 +1,91 @@
-class LiterateTest
-  attr_reader :code
+require 'temple'
 
-  def initialize(file)
-    lines = File.readlines(file)
-    exp = parse(lines)
-    @code = "require 'helper'\n\n" + compile(exp, "  include Helper\n\n")
-  end
-
-  def parse(lines)
-    stack = []
-    until lines.empty?
-      line = lines.shift
-      case line
-      when /\A(#+)\s*(.*)\Z/
-        while stack.size >= $1.size
-          stack.pop
-        end
-        exp = [:section, $2]
-        stack.last << exp if stack.last
-        stack << exp
-      when /\A~{3,}\s*(\w+)\s\Z/
-        lang = $1
-        block = []
-        until lines.empty?
-          line = lines.shift
-          case line
-          when /\A~{3,}\Z/
-            break
-          when /\A.*\Z/
-            block << $&
+class LiterateTest < Temple::Engine
+  class Parser < Temple::Parser
+    def call(lines)
+      stack = [[:multi]]
+      until lines.empty?
+        case lines.shift
+        when /\A(#+)\s*(.*)\Z/
+          while stack.size - 1 >= $1.size
+            stack.pop
           end
+          block = [:multi]
+          stack.last << [:section, $2, block]
+          stack << block
+        when /\A~{3,}\s*(\w+)\s*\Z/
+          lang = $1
+          block = []
+          until lines.empty?
+            case lines.shift
+            when /\A~{3,}\Z/
+              break
+            when /\A.*\Z/
+              block << $&
+            end
+          end
+          stack.last << [lang.to_sym, block.join("\n")]
+        when /\A\s*\Z/
+        when /\A.*\Z/
+          stack.last << [:comment, $&]
         end
-        stack.last << [lang.to_sym, block.join("\n")]
-      when /\A\s+\Z/
-      when /\A.*\Z/
-        stack.last << [:comment, $&]
       end
+      stack.first
     end
-    stack.first
   end
 
-  def compile(exp, preamble = '')
-    case exp.first
-    when :section
-      begin
-        old_options = @options
-        raise 'Section inside of test case' if @in_testcase
-        type, title, *rest = exp
-        rest = rest.map {|exp| compile(exp) }.join("\n").gsub("\n", "\n  ")
-        "describe #{title.inspect} do\n#{preamble}  #{rest}\nend\n"
-      ensure
-        @options = old_options
-      end
-    when :comment
-      "#{@in_testcase ? '  ' : ''}# #{exp.last}"
-    when :slim
-      puts exp.last
+  class Compiler < Temple::Filter
+    def call(exp)
+      @opts = nil
+      @in_testcase = nil
+      @level = 0
+      "require 'helper'\n\n" << compile(exp)#, "  include Helper\n\n")
+    end
+
+    def on_section(title, body)
+      old_options = @opts
+      @level += 1
+      raise 'Section inside of test case' if @in_testcase
+      result = "describe #{title.inspect} do\n  "
+      result << "include Helper\n  " if @level == 1
+      result << compile(body).gsub("\n", "\n  ") << "\nend\n"
+    ensure
+      @opts = old_options
+      @level -= 1
+    end
+
+    def on_multi(*exps)
+      exps.map {|exp| compile(exp) }.join("\n")
+    end
+
+    def on_comment(text)
+      "#{@in_testcase ? '  ' : ''}# #{text}"
+    end
+
+    def on_slim(code)
       raise 'Two slim blocks inside test case' if @in_testcase
       @in_testcase = true
-      "it 'should render' do\n  slim = #{exp.last.inspect}"
-    when :html
+      "it 'should render' do\n  slim = #{code.inspect}"
+    end
+
+    def on_html(code)
       raise 'HTML block must come after slim block' unless @in_testcase
       @in_testcase = false
-      result =  "  html = #{exp.last.inspect}\n"
-      result << "  options = {#{@options}}\n" if @options
-      result << "  assert_html(html, slim#{@options && ', options'})\nend\n"
-    when :options
+      result =  "  html = #{code.inspect}\n"
+      result << "  options = {#{@opts}}\n" if @opts
+      result << "  assert_html(html, slim#{@opts && ', options'})\nend\n"
+    end
+
+    def on_options(code)
       raise 'Options set inside test case' if @in_testcase
-      @options = exp.last
-      "# #{@options.gsub("\n", "\n# ")}"
+      @opts = code
+      "# #{@opts.gsub("\n", "\n# ")}"
     end
   end
 
-  def run
-    puts code
-    eval(code)
-  end
+  use Parser
+  use Compiler
+  use(:Evaluator) {|code| eval(code) }
 end
 
-LiterateTest.new(File.join(File.dirname(__FILE__), 'TESTS.md')).run
+LiterateTest.new.call(File.readlines(File.join(File.dirname(__FILE__), 'TESTS.md')))
