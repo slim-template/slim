9f6dc7c5d51724aa7f038334f3d745c47921c818
Daniel Mendler <mail@daniel-mendler.de>
2013-01-05 22:29:36 +0100

    update rails tests for rails4

--- test/rails/config/environments/test.rb
+++ test/rails/config/environments/test.rb
@@ -7,9 +7,6 @@ Dummy::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
--- test/rails/config/routes.rb
+++ test/rails/config/routes.rb
@@ -56,5 +56,5 @@ Dummy::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))'
+  get ':controller(/:action(/:id(.:format)))'
 end
