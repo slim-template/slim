61c4be05b946776b0cce2f0756818617fef47910
Daniel Mendler <mail@daniel-mendler.de>
2013-04-03 23:17:58 +0200

    better html_safe tests copied from temple

--- test/core/test_code_escaping.rb
+++ test/core/test_code_escaping.rb
@@ -19,18 +19,22 @@ p = "<strong>Hello World\\n, meet \\"Slim\\"</strong>."
 
   def test_render_with_html_safe_false
     source = %q{
-p = HtmlUnsafeString.new("<strong>Hello World\\n, meet \\"Slim\\"</strong>.")
+p = "<strong>Hello World\\n, meet \\"Slim\\"</strong>."
 }
 
-    assert_html "<p>&lt;strong&gt;Hello World\n, meet \&quot;Slim\&quot;&lt;/strong&gt;.</p>", source, :use_html_safe => true
+    with_html_safe do
+      assert_html "<p>&lt;strong&gt;Hello World\n, meet \&quot;Slim\&quot;&lt;/strong&gt;.</p>", source, :use_html_safe => true
+    end
   end
 
   def test_render_with_html_safe_true
     source = %q{
-p = HtmlSafeString.new("<strong>Hello World\\n, meet \\"Slim\\"</strong>.")
+p = "<strong>Hello World\\n, meet \\"Slim\\"</strong>.".html_safe
 }
 
-    assert_html "<p><strong>Hello World\n, meet \"Slim\"</strong>.</p>", source, :use_html_safe => true
+    with_html_safe do
+      assert_html "<p><strong>Hello World\n, meet \"Slim\"</strong>.</p>", source, :use_html_safe => true
+    end
   end
 
   def test_render_with_disable_escape_false
@@ -49,5 +53,59 @@ p = HtmlSafeString.new("<strong>Hello World\\n, meet \\"Slim\\"</strong>.")
 }
 
     assert_html "<p>Hello</p><p>World</p>", source, :disable_escape => true
+  end
+
+  def test_escaping_evil_method_with_pretty
+    source = %q{
+p = evil_method
+}
+
+    assert_html "<p>\n  &lt;script&gt;do_something_evil();&lt;/script&gt;\n</p>", source, :pretty => true
+  end
+
+  def test_render_without_html_safe_with_pretty
+    source = %q{
+p = "<strong>Hello World\\n, meet \\"Slim\\"</strong>."
+}
+
+    assert_html "<p>\n  &lt;strong&gt;Hello World\n  , meet \&quot;Slim\&quot;&lt;/strong&gt;.\n</p>", source, :pretty => true
+  end
+
+  def test_render_with_html_safe_false_with_pretty
+    source = %q{
+p = "<strong>Hello World\\n, meet \\"Slim\\"</strong>."
+}
+
+    with_html_safe do
+      assert_html "<p>\n  &lt;strong&gt;Hello World\n  , meet \&quot;Slim\&quot;&lt;/strong&gt;.\n</p>", source, :use_html_safe => true, :pretty => true
+    end
+  end
+
+  def test_render_with_html_safe_true_with_pretty
+    source = %q{
+p = "<strong>Hello World\\n, meet \\"Slim\\"</strong>.".html_safe
+}
+
+    with_html_safe do
+      assert_html "<p>\n  <strong>Hello World\n  , meet \"Slim\"</strong>.\n</p>", source, :use_html_safe => true, :pretty => true
+    end
+  end
+
+  def test_render_with_disable_escape_false_with_pretty
+    source = %q{
+= "<p>Hello</p>"
+== "<p>World</p>"
+}
+
+    assert_html "&lt;p&gt;Hello&lt;/p&gt;<p>World</p>", source, :pretty => true
+  end
+
+  def test_render_with_disable_escape_true_with_pretty
+    source = %q{
+= "<p>Hello</p>"
+== "<p>World</p>"
+}
+
+    assert_html "<p>Hello</p><p>World</p>", source, :disable_escape => true, :pretty => true
   end
 end
