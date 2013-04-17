8097a23bccfb33838ca593e52d9b43313ff10065
Daniel Mendler <mail@daniel-mendler.de>
2013-04-17 02:33:44 +0200

    add missing tests for do keyword generation

--- test/core/test_code_blocks.rb
+++ test/core/test_code_blocks.rb
@@ -11,10 +11,30 @@ p
     assert_html '<p>Hello Ruby! Hello from within a block! Hello Ruby!</p>', source
   end
 
+  def test_render_with_output_code_block_without_do
+    source = %q{
+p
+  = hello_world "Hello Ruby!"
+    | Hello from within a block!
+}
+
+    assert_html '<p>Hello Ruby! Hello from within a block! Hello Ruby!</p>', source
+  end
+
   def test_render_with_output_code_within_block
     source = %q{
 p
   = hello_world "Hello Ruby!" do
+    = hello_world "Hello from within a block!"
+}
+
+    assert_html '<p>Hello Ruby! Hello from within a block! Hello Ruby!</p>', source
+  end
+
+  def test_render_with_output_code_within_block_without_do
+    source = %q{
+p
+  = hello_world "Hello Ruby!"
     = hello_world "Hello from within a block!"
 }
 
@@ -26,6 +46,17 @@ p
 p
   = hello_world "Hello Ruby!" do
     = hello_world "Hello from within a block!" do
+      = hello_world "And another one!"
+}
+
+    assert_html '<p>Hello Ruby! Hello from within a block! And another one! Hello from within a block! Hello Ruby!</p>', source
+  end
+
+  def test_render_with_output_code_within_block_2_without_do
+    source = %q{
+p
+  = hello_world "Hello Ruby!"
+    = hello_world "Hello from within a block!"
       = hello_world "And another one!"
 }
 
@@ -56,9 +87,29 @@ p
     assert_html '<p>Hey!Hey!Hey!</p>', source
   end
 
+  def test_render_with_control_code_loop_without_do
+    source = %q{
+p
+  - 3.times
+    | Hey!
+}
+
+    assert_html '<p>Hey!Hey!Hey!</p>', source
+  end
+
   def test_captured_code_block_with_conditional
     source = %q{
 = hello_world "Hello Ruby!" do
+  - if true
+    | Hello from within a block!
+}
+
+    assert_html 'Hello Ruby! Hello from within a block! Hello Ruby!', source
+  end
+
+  def test_captured_code_block_with_conditional_without_do
+    source = %q{
+= hello_world "Hello Ruby!"
   - if true
     | Hello from within a block!
 }
