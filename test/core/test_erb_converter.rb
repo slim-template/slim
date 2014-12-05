aa6a2591c268e4e0903162b786fb4d9b964923bd
Daniel Mendler <mail@daniel-mendler.de>
2014-12-05 11:52:49 +0100

    update erb converter test

--- test/core/test_erb_converter.rb
+++ test/core/test_erb_converter.rb
@@ -31,36 +31,36 @@ html
       p= "dynamic text with\nnewline"
 }
 
-    result = %q{<%
-%><!DOCTYPE html><%
-%><html><%
-%><head><%
-%><title>Hello World!</title><%
-%><!--Meta tags<%
-%>
-with long explanatory<%
-%>
-multiline comment--><%
-%><meta content="template language" name="description" /><%
-%><!--Stylesheets--><%
-%><link href="style.css" media="screen" rel="stylesheet" type="text/css" /><%
-%><link href="colors.css" media="screen" rel="stylesheet" type="text/css" /><%
-%><!--Javascripts--><%
-%><script src="jquery.js"><%
-%></script><script src="jquery.ui.js"><%
-%></script><!--[if lt IE 9]><%
-%><script src="old-ie1.js"><%
-%></script><script src="old-ie2.js"><%
-%></script><![endif]--><style type="text/css">body{background-color:red}<%
-%><%
-%></style><%
-%></head><body><%
-%><div id="container"><%
-%><p>Hello<%
-%>
-World!</p><%
-%><p><%= ::Temple::Utils.escape_html(("dynamic text with\nnewline")) %><%
-%></p></div></body></html>}
+    result = %q{
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello World!</title>
+<!--Meta tags
+
+with long explanatory
+
+multiline comment-->
+<meta content="template language" name="description" />
+<!--Stylesheets-->
+<link href="style.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="colors.css" media="screen" rel="stylesheet" type="text/css" />
+<!--Javascripts-->
+<script src="jquery.js">
+</script><script src="jquery.ui.js">
+</script><!--[if lt IE 9]>
+<script src="old-ie1.js">
+</script><script src="old-ie2.js">
+</script><![endif]--><style type="text/css">body{background-color:red}
+
+</style>
+</head><body>
+<div id="container">
+<p>Hello
+
+World!</p>
+<p><%= ::Temple::Utils.escape_html(("dynamic text with\nnewline")) %>
+</p></div></body></html>}
 
     assert_equal result, Slim::ERBConverter.new.call(source)
   end
