8f3071551c2d5c0847e1a9ab04ba53a8b246ef25
Daniel Mendler <mail@daniel-mendler.de>
2014-12-23 21:56:07 +0100

    unicode compatible regexp

--- lib/slim/command.rb
+++ lib/slim/command.rb
@@ -59,7 +59,7 @@ module Slim
 
       opts.on('-l', '--locals Hash|YAML|JSON', String, 'Set local variables') do |locals|
         @options[:locals] =
-          if locals =~ /\A\s*\{\s*\w+:/
+          if locals =~ /\A\s*\{\s*\p{Word}+:/
             eval(locals)
           else
             require 'yaml'
