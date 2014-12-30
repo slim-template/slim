31c8f448247bd0d8f460059c65c80fab95228404
Dominik Menke <dominik.menke@gmail.com>
2014-12-31 00:23:34 +0100

    Workaround for JRuby failure
    As of
    
    - https://github.com/jruby/jruby/issues/1243 and
    - https://github.com/jruby/jruby/issues/1789,
    
    there might be a regression in JRuby 1.7.x, while JRuby 9000 works
    just fine.

--- test/rails/test/test_slim.rb
+++ test/rails/test/test_slim.rb
@@ -46,11 +46,17 @@ class TestSlim < ActionDispatch::IntegrationTest
     assert_html "<h1>Hello Slim!</h1><p>With a partial!</p>"
   end
 
-  puts 'Streaming test enabled'
-  test "streaming" do
-    get "/slim/streaming"
-    output = "2f\r\n<!DOCTYPE html><html><head><title>Dummy</title>\r\nd\r\n</head><body>\r\n17\r\nHeading set from a view\r\n15\r\n<div class=\"content\">\r\n53\r\n<p>Page content</p><h1><p>Hello Streaming!</p></h1><h2><p>Hello Streaming!</p></h2>\r\n14\r\n</div></body></html>\r\n0\r\n\r\n"
-    assert_equal output, @response.body
+  if RUBY_ENGINE == 'jruby' && RUBY_ENGINE < '2.2.0'
+    puts 'Streaming test disabled for JRuby < 9000.',
+      'See https://github.com/jruby/jruby/issues/1243',
+      'and https://github.com/jruby/jruby/issues/1789'
+  else
+    puts 'Streaming test enabled.'
+    test "streaming" do
+      get "/slim/streaming"
+      output = "2f\r\n<!DOCTYPE html><html><head><title>Dummy</title>\r\nd\r\n</head><body>\r\n17\r\nHeading set from a view\r\n15\r\n<div class=\"content\">\r\n53\r\n<p>Page content</p><h1><p>Hello Streaming!</p></h1><h2><p>Hello Streaming!</p></h2>\r\n14\r\n</div></body></html>\r\n0\r\n\r\n"
+      assert_equal output, @response.body
+    end
   end
 
   test "render integers" do
