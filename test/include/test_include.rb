9db075ea8c05337633d4c56ad4cebfa14f25a018
Daniel Mendler <mail@daniel-mendler.de>
2015-06-02 12:36:52 +0200

    test for #624

--- test/include/test_include.rb
+++ test/include/test_include.rb
@@ -4,12 +4,13 @@ require 'slim/include'
 class TestSlimInclude < TestSlim
   def test_include
     source = %q{
+br/
 a: include slimfile
 b: include textfile
 c: include slimfile.slim
 d: include subdir/test
 }
-    assert_html '<a>slim1recslim2</a><b>1+2=3</b><c>slim1recslim2</c><d>subdir</d>', source, include_dirs: [File.expand_path('files', File.dirname(__FILE__))]
+    assert_html '<br /><a>slim1recslim2</a><b>1+2=3</b><c>slim1recslim2</c><d>subdir</d>', source, include_dirs: [File.expand_path('files', File.dirname(__FILE__))]
   end
 
   def test_include_with_newline
