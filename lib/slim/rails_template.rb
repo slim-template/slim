f8bf2d61f0e87568bdf2d8a6e4f4670c286655d8
Dieter Lunn <coder2000@gmail.com>
2021-10-16 20:59:38 -0500

    feat: add rails template to support annotation

---
+++ lib/slim/rails_template.rb
@@ -0,0 +1,39 @@
+require "temple"
+require "slim/engine"
+
+module Slim
+  class RailsTemplate
+    class << self
+      def options
+        @options ||= {
+          generator: Temple::Generators::RailsOutputBuffer,
+          use_html_safe: true,
+          streaming: true,
+          buffer_class: "ActionView::OutputBuffer"
+        }
+      end
+
+      def set_options(opts)
+        options.update(opts)
+      end
+    end
+
+    def call(template, source = nil)
+      source ||= template.source
+      options = RailsTemplate.options
+
+      if ActionView::Base.try(:annotate_rendered_view_with_filenames) && template.format == :html
+        options[:preamble] = "<!-- BEGIN #{template.short_identifier} -->\n"
+        options[:postamble] = "<!-- END #{template.short_identifier} -->\n"
+      end
+
+      Slim::Engine.new(options).call(source)
+    end
+
+    def supports_streaming?
+      RailsTemplate.options[:streaming]
+    end
+  end
+
+  ActionView::Template.register_template_handler :slim, RailsTemplate.new
+end
