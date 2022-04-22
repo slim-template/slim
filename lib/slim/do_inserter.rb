b238f738382ceac136b9e7759397c11f4a433a83
Henrik Nyh and Jedrzej Lagodzinski <devs+henrik+JensRay@auctionet.com>
2022-04-22 13:21:42 +0200

    Add support for 'case/in'
    Only supported in Ruby 2.7+, and .travis.yml also runs tests in 2.6, so exclude the test in unsupported Rubies.

--- lib/slim/do_inserter.rb
+++ lib/slim/do_inserter.rb
@@ -7,7 +7,7 @@ module Slim
   #
   # @api private
   class DoInserter < Filter
-    BLOCK_REGEX = /(\A(if|unless|else|elsif|when|begin|rescue|ensure|case)\b)|\bdo\s*(\|[^\|]*\|\s*)?\Z/
+    BLOCK_REGEX = /(\A(if|unless|else|elsif|when|in|begin|rescue|ensure|case)\b)|\bdo\s*(\|[^\|]*\|\s*)?\Z/
 
     # Handle control expression `[:slim, :control, code, content]`
     #
