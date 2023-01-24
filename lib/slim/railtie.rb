e7412233eb6edb8aece5af58356e456cd8d7359b
Michael Baudino <michael@baudi.no>
2023-01-24 10:16:21 +0100

    Fix Railtie load error (#904)
    This commit fixes an error when requiring `slim` via `slim-rails`:
    
    ```
    uninitialized constant Slim::Rails::Railtie (NameError)
    
      class Railtie < Rails::Railtie
    ```

--- lib/slim/railtie.rb
+++ lib/slim/railtie.rb
@@ -1,5 +1,5 @@
 module Slim
-  class Railtie < Rails::Railtie
+  class Railtie < ::Rails::Railtie
     initializer "initialize slim template handler" do
       ActiveSupport.on_load(:action_view) do
         Slim::RailsTemplate = Temple::Templates::Rails(Slim::Engine,
